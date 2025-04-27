6869a70923018a734c1ee3c505a01879fed76a29
Konstantin <kastett@mail.ru>
2025-04-27 21:20:25 +0300

    cd and gitignor

--- LIFTEH_project/LIFTEH_project/settings.py
+++ LIFTEH_project/LIFTEH_project/settings.py
@@ -110,14 +110,15 @@ STATIC_URL = 'static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = '/login/'
-
-# STATIC_URL = '/static/'
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 INTERNAL_IPS = ['127.0.0.1',]
