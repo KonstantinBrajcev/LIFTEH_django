94fcfbe70205b29f3f7bd52875293f641aa02beb
Konstantin <kastett@mail.ru>
2025-04-30 03:25:51 +0300

    Sleep

--- LIFTEH_project/LIFTEH_project/settings_prod.py
+++ LIFTEH_project/LIFTEH_project/settings_prod.py
@@ -1,8 +1,17 @@
 from .settings import *  # Импорт общих настроек
+import os
 
+# Базовые настройки
 DEBUG = False
 ALLOWED_HOSTS = ['jelezo.by', '178.159.242.118', 'www.jelezo.by']
+
+# Настройки статических файлов
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Настройки WhiteNoise
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+WHITENOISE_MANIFEST_STRICT = False  # Для избежания ошибок при отсутствии файлов
 
 # Middleware для продакшена
 MIDDLEWARE = [
@@ -15,3 +24,11 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+# Дополнительные настройки безопасности
+SECURE_HSTS_SECONDS = 31536000  # 1 year
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
